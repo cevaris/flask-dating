f35d526feebb1ee6abfae8c9f15cfd3a61aa5053
Cevaris <cevaris@gmail.com>
2014-02-24 21:38:44 -0700

    got working

--- dating/models.py
+++ dating/models.py
@@ -4,32 +4,6 @@ from dating import db
 from slugify import slugify
 import random
 
-# class Album(db.Document):
-#     slug = db.StringField(max_length=255, required=False)
-#     name = db.StringField(max_length=255, required=True)
-#     publisher = db.StringField(max_length=255, required=True)
-#     genre = db.StringField(max_length=255, required=True)
-#     media_type = db.StringField(max_length=255, required=True)
-#     name = db.StringField(max_length=255, required=True)
-    
-#     def get_absolute_url(self):
-#         return url_for('album', kwargs={"slug": self.slug})
-
-#     def __unicode__(self):
-#         return self.name
-
-
-
-# class User(db.Document):
-
-#     name = db.StringField(max_length=255, required=True)
-#     slug = db.StringField(max_length=255, required=False)
-#     email = db.StringField(max_length=255, required=False)
-
-#     def __unicode__(self):
-#         return "%s, Email:%s" % (self.name, self.email)
-
-
 class Pets(db.Document):
 
     name = db.StringField(max_length=255, required=True, unique=True)
--- dating/views.py
+++ dating/views.py
@@ -38,42 +38,17 @@ class Home(MethodView):
   def get(self):
     return render_template('dating/home.html')
 
-class Users(MethodView):  
+class ShowArticle(MethodView):
 
-  def get(self):
-    users = User.objects.all()
-    return render_template('dating/users.html',users=users)
-
-# class CreateArticle(MethodView):
-
-#   def get(self):
-#     return render_template('articles/create.html')
-
-#   def post(self):
-
-#     if request.method == 'POST':
-#       body = request.form['body']
-#       author = request.form['body']
-#       title = request.form['body']
-#       slug = slugify(title)
-#       article = Article(body=body, author=author, title=title, slug=slug)
-#       article.save()
-
-#       return render_template('articles/show.html', article=article)
-#     return render_template('articles/create.html')
-
-# class ShowArticle(MethodView):
-
-#   def get(self, slug):
-#     article = Article.objects.get_or_404(slug=slug)
-#     return render_template('articles/show.html', article=article)
+  def get(self, slug):
+    pet = Pets.objects.get_or_404(slug=slug)
+    return render_template('dating/show.html', pet=pet)
 
 
 
 dating.add_url_rule('/', view_func=Home.as_view('home'))
-dating.add_url_rule('/users', view_func=Users.as_view('users'))
 dating.add_url_rule('/search', view_func=Search.as_view('search'))
-# articles.add_url_rule('/create', view_func=CreateArticle.as_view('create'))
-# articles.add_url_rule('/view/<slug>/', view_func=ShowArticle.as_view('show'))
+dating.add_url_rule('/pet/<slug>/', view_func=ShowArticle.as_view('show'))
+
 
 
